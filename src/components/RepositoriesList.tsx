11860ccb8e9a7c8e59eb0df2b2c0adf71e979731
Stanislav Salnikov <delfi89@gmail.com>
2021-04-15 21:53:13 +0300

    Refactored code

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,21 +1,14 @@
 import React, {useState} from "react";
-// import {useDispatch} from "react-redux";
-// import {useSelector} from "react-redux";
-// import {RootState} from "../state";
-// import {actionCreators} from '../state'
 
 import {useActions} from "../hooks/useActions";
 import {useTypedSelector} from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('')
-    // const dispatch = useDispatch();
     const {searchRepositories} = useActions();
-    // const {data, error, loading} = useSelector(((state: RootState) => state.repositories))
     const {data, error, loading} = useTypedSelector(state => state.repositories);
     const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
         event.preventDefault();
-        // dispatch(actionCreators.searchRepositories(term))
         searchRepositories(term);
     }
 
@@ -23,6 +16,9 @@ const RepositoriesList: React.FC = () => {
         <form onSubmit={onSubmit}>
             <input onChange={e => setTerm(e.target.value)} value={term}/>
             <button>Search</button>
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading...</h3>}
+            {!error && !loading && data.map(name => <div key={name}>{name}</div>)}
         </form>
     </div>
 }
